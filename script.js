09f3c81bb2950baff4b6ad31e930334ad7e5da61
Daniel Bento <danielconegundesbehnto2003@gmail.com>
2023-02-27 12:17:24 -0400

    Adicionei os arquivos
    Adicionei os arquivos de código

---
+++ script.js
@@ -0,0 +1,20 @@
+const hora = document.getElementById('hora')
+const min = document.getElementById('min')
+const seg = document.getElementById('seg')
+
+
+setInterval(()=>{
+    let date = new Date();
+    let horas = date.getHours();
+    let minutos = date.getMinutes();
+    let segundos = date.getSeconds();
+
+    hora.innerHTML = `${formatTime(horas)}`
+    min.innerHTML = `${formatTime(minutos)}`
+    seg.innerHTML = `${formatTime(segundos)}`
+
+},1000);
+
+function formatTime(time){
+    return time < 10 ? '0' + time : time;
+}
